3e0c44794414ea1e27eda01a8a04ef9e3d74dfaf
avd <aravind19112003@gmail.com>
2023-11-26 23:08:50 +0530

    Maximum of array

---
+++ src/Max.java
@@ -0,0 +1,15 @@
+public class Max {
+    public static void main(String[] args) {
+        int[] arr = {1,56,78,23,99,21};
+        System.out.println(max(arr));
+    }
+    static int max(int[] arr){
+        int max = arr[0];
+        for (int i=1 ; i < arr.length ; i++){
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
