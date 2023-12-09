8250d9fad2d8b9713ac1ff224b0cca27951444d5
avd <aravind19112003@gmail.com>
2023-12-09 12:46:50 +0530

    Binary Search

---
+++ src/BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-4,-3,-1,0,2,3,5,6,7,9,10,11};
+        int target = 6;
+        int ans = search(arr , target);
+        System.out.println(ans);
+    }
+
+    static int search(int[] arr , int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end){
+            int mid = start + (end - start)/2 ;
+            if (target < arr[mid]){
+                end = mid-1;
+            } else if (target > arr[mid]) {
+                start = mid+1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
