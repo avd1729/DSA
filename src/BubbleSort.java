3caba8334ee5528b472920effe0ec2a937fd939a
avd <aravind19112003@gmail.com>
2024-01-03 00:13:37 +0530

    Bubble sort (swapped)

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -9,15 +9,22 @@ public class BubbleSort {
     }
     // in-place sort
     static void sort(int[] arr){
+        boolean swapped;
         // for each step , max item will come at the last respective index.
         for (int i = 0; i < arr.length; i++) {
+            swapped = false;
             for (int j = 1; j < arr.length - i ; j++) {
                 if (arr[j] < arr[j-1]){
                     // swap
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    swapped = true;
                 }
+            }
+            // if you did not swap for a particular value of i , it means the array is sorted , hence stop the program
+            if(!swapped){
+                break;
             }
         }
     }
