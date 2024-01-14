b5de9d98ce4eedc863bb73859394d4b910d0af6d
avd <aravind19112003@gmail.com>
2024-01-15 00:29:55 +0530

    Set mismatch

---
+++ src/SetMismatch.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class SetMismatch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,4};
+        ArrayList<Integer> ans = finder(arr);
+        System.out.println(ans);
+    }
+
+    static ArrayList<Integer> finder(int[] arr){
+        sort(arr);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i+1 ){
+                list.add(arr[i]);
+                list.add(i+1);
+                //return list;
+            }
+        }
+        return list;
+    }
+
+    static void sort(int[] arr){
+        int i = 0;
+        while (i < arr.length){
+            if (arr[i] < arr.length && arr[i] != arr[arr[i]-1]){
+                swap(arr , i , arr[i]);
+            }
+            else{
+                i++;
+            }
+        }
+
+    }
+
+    static void swap(int[] arr , int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
