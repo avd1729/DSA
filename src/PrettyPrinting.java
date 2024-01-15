88f16d491a1b5d4bdeb43b4a4c690b804ff9ca3a
avd <aravind19112003@gmail.com>
2024-01-15 14:35:36 +0530

    Placeholders

---
+++ src/PrettyPrinting.java
@@ -0,0 +1,8 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 12.3455f;
+        //System.out.printf("Formatted number is %.2f",a); // 12.35 not 12.34
+        //System.out.printf("Formatted number is %.3f",Math.PI);
+        System.out.printf("Hello , my name is %s , i am %s","avd","cool");
+    }
+}
