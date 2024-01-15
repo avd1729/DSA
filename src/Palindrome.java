202f8f3d28b6d990ea39701aef8797aecfcbfed5
avd <aravind19112003@gmail.com>
2024-01-15 15:14:27 +0530

    Palindrome using String

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -5,6 +5,7 @@ public class Palindrome {
         System.out.println(ans);
     }
     static boolean palindrome(String a){
+        a = a.toLowerCase();
         int start = 0;
         int end = a.length()-1;
         while (start <= end){
