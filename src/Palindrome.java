3c16014832cf70cfba8a8a8cfbfe9eacad156171
avd <aravind19112003@gmail.com>
2024-01-15 15:13:23 +0530

    Palindrome using String

---
+++ src/Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String word = "malayalam";
+        boolean ans = palindrome(word);
+        System.out.println(ans);
+    }
+    static boolean palindrome(String a){
+        int start = 0;
+        int end = a.length()-1;
+        while (start <= end){
+            if (a.charAt(start) == a.charAt(end)){
+                start++;
+                end--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
