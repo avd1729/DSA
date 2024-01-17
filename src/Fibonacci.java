06e36370c923d750fb3d1186d7108267458bf683
avd <aravind19112003@gmail.com>
2024-01-17 15:33:02 +0530

    Fibonacci using recurrence formula

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -4,15 +4,21 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int a = 0;
-        int b = 1;
-        int count = 2;
+        long a = 0;
+        long b = 1;
+        long count = 2;
         while (count <= n){
-            int temp = b;
+            long temp = b;
             b = b+a;
             a = temp;
             count++;
         }
         System.out.println(b);
+
+        System.out.println(fibformula(n));
+    }
+
+    static long fibformula(int n){
+        return (long) ((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
     }
 }
