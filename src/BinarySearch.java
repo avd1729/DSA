02809435672a6b0e2acb55ff21d71558fc395838
avd <aravind19112003@gmail.com>
2024-01-19 00:45:28 +0530

    Binary Search using recursion

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,20 +1,26 @@
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {-4,-3,-1,0,2,3,5,6,7,9,10,11};
+
+        int[] arr = {-4, -3, -1, 0, 2, 3, 5, 6, 7, 9, 10, 11};
         int target = 6;
-        int ans = search(arr , target);
-        System.out.println(ans);
+        int ans1 = BinarySearchIterative(arr, target);
+        System.out.println(ans1);
+        int ans2 = BinarySearchRecursive(arr, 0, arr.length - 1, target);
+        System.out.println(ans2);
     }
 
-    static int search(int[] arr , int target){
+    static int BinarySearchIterative(int[] arr, int target) {
+
         int start = 0;
         int end = arr.length - 1;
-        while (start <= end){
-            int mid = start + (end - start)/2 ;
-            if (target < arr[mid]){
-                end = mid-1;
-            } else if (target > arr[mid]) {
-                start = mid+1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            }
+            else if (target > arr[mid]) {
+                start = mid + 1;
             }
             else {
                 return mid;
@@ -22,4 +28,24 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    static int BinarySearchRecursive(int[] arr, int start, int end, int target) {
+
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        if (target < arr[mid]) {
+            return BinarySearchRecursive(arr, start, mid - 1, target);
+        }
+        else if (target > arr[mid]) {
+            return BinarySearchRecursive(arr, mid + 1, end, target);
+        }
+        else {
+            return mid;
+        }
+    }
+
 }
