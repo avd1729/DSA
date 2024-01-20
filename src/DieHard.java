febf80f7141e00c64bfaac49584fb14366ca873a
avd <aravind19112003@gmail.com>
2024-01-21 01:12:57 +0530

    DieHard

---
+++ src/DieHard.java
@@ -0,0 +1,10 @@
+public class DieHard {
+    public static void main(String[] args) {
+
+    }
+    // gcd(a,b) = gcd(rem(b/a),a)
+    static boolean isDieHard(int jug1 , int jug2 , int target){
+        return true;
+    }
+
+}
