a52e3108a68d10dd19e9aa07695c9d991da77f1f
avd <aravind19112003@gmail.com>
2024-01-24 15:43:38 +0530

    Subsets with duplicates

--- src/Subset.java
+++ src/Subset.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Subset {
     public static void main(String[] args) {
         subsets1("","abc");
         System.out.println(subsets2("","abc"));
-        int[] arr = {1,2,3};
-        List<List<Integer>> ans = subsets3(arr);
-        System.out.println(ans);
-
+        System.out.println(subsets3(new int[] {1,2,3}));
+        System.out.println(subsets4(new int[] {1,2,2}));
     }
 
     static void subsets1 (String p , String up){
@@ -61,6 +60,41 @@ public class Subset {
             for (int i = 0; i < size; i++) {
                 List<Integer> inner = new ArrayList<>(list.get(i));
                 inner.add(num);
+                list.add(inner);
+            }
+        }
+
+        return list;
+    }
+
+    // Duplicates
+
+    /* when you find a duplicate , only add it in the newly created subset of previous step
+    duplicates have to be together
+    */
+
+    static List<List<Integer>> subsets4(int[] arr){
+
+        Arrays.sort(arr);
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        list.add(new ArrayList<>());
+
+        int s = 0;
+        int e = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            // if current and previous element are same , s = e+1
+
+            if (i > 0 && arr[i] == arr[i-1]){
+                s = e + 1;
+            }
+            e = list.size()-1;
+            int n = list.size();
+            for (int j = s; j < n; j++) {
+                List<Integer> inner = new ArrayList<>(list.get(j));
+                inner.add(arr[i]);
                 list.add(inner);
             }
         }
